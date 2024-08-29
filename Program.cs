590262743ac4113e73f441ac46176f973b554257
Emmanuel Antônio <Emmanuel_Braga@outlook.com>
2024-08-28 21:17:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
+                var newPost = new Post()
+                {
+                    Title = "Test Requisition POST",
+                    Body = "Hello World",
+                    UserId = 11
+                };
+                var newPostJson = JsonConvert.SerializeObject(newPost); 
+                var payload = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+                var result = client.PostAsync(endpoint, payload).Result.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
